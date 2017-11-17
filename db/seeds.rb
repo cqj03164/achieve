332bd48ffebd04fa789598519a8d43c3ee3e0d14
Hirotaka Ito <h-itou@nttpc.co.jp>
2017-11-17 11:42:39 +0000

    20171117-dive9-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-n = 1
-while n <= 100
-  Blog.create(
-    title: "投稿#{n}回目",
-    content: "中身#{n}回目",
-    user_id: n
-  )
-  n = n + 1
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: "サンプルユーザ#{n}",
+               )
+               
 end
