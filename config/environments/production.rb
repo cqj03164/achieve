90888189e99f0f889f854588fa4ac29a3c988e4b
Hirotaka Ito <h-itou@nttpc.co.jp>
2017-11-21 08:42:19 +0000

    heroku-new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-config.action_mailer.default_url_options = { host: 'https://nameless-scrubland-24779.git' }
+config.action_mailer.default_url_options = { host: ' https://git.heroku.com/nameless-scrubland-24779.git' }
   
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
