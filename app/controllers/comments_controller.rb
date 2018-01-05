92d1798b5c5c9525cdb2020edb9403133898c425
Hirotaka Ito <h-itou@nttpc.co.jp>
2018-01-05 06:24:59 +0000

    20180105

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
-    # クライアント要求に応じてフォーマットを変更
+   
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
@@ -13,8 +13,9 @@ class CommentsController < ApplicationController
       else
         format.html { render :new }
       end
+    end
   end
-  
+ 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
@@ -24,15 +25,16 @@ class CommentsController < ApplicationController
       else
         format.html { render :new }
       end
-    
+    end
   end
-    
+ 
 
   private
-    # ストロングパラメーター
+  
     def comment_params
-      params.require(:comment).permit(:blog_id, :content)
-　　end
-　end
+      params.require(:comment).permit(:blog_id, :content )
 
-
+    end
+  
+  
+end
